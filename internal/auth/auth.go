5dac5962271b7d714324ade5dad8d9bfd20799ad
Loren Barcus <loren.barcus@gmail.com>
2026-02-21 00:55:23 -0600

    Fix tests

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -19,6 +19,5 @@ func GetAPIKey(headers http.Header) (string, error) {
 		return "", errors.New("malformed authorization header")
 	}
 
-	// TODO: fix
-	return splitAuth[1] + "oops", nil
+	return splitAuth[1], nil
 }
